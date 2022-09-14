47b6c86fa7504bc16de4ee71994e85ac0d06934f
Elizabeth Rodricks <elizabethrodricks@Elizabeths-iMac.local>
2022-09-14 13:54:33 -0400

    test

---
+++ src/components/Firebase.js
@@ -0,0 +1,25 @@
+
+// import Firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import 'firebase/compat/firestore';
+// import "firebase/compat/database";
+// import "firebase/compat/storage";
+//
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBKI05JE-gn_DptXTd3nyP6d6WDtRR7mK0",
+//   authDomain: "dev-checklist-68d84.firebaseapp.com",
+//   databaseURL: "https://dev-checklist-68d84-default-rtdb.firebaseio.com",
+//   projectId: "dev-checklist-68d84",
+//   storageBucket: "dev-checklist-68d84.appspot.com",
+//   messagingSenderId: "371448344789",
+//   appId: "1:371448344789:web:aac49148a6c42dfd8cb25c",
+//   measurementId: "G-2HR7ZMJETX"
+// };
+//
+//
+//
+// const firebase = Firebase.initializeApp(firebaseConfig);
+//
+// const db = firebase.firestore();
+//
+// export { firebase, db };
